8bfa8116375024ef8affb87df283796bad35d6c0
SayeemZaman3 <sayeem312@gmail.com>
2025-06-13 17:59:50 +0600

    mobile image slider

--- script.js
+++ script.js
@@ -14,3 +14,32 @@ $(document).click((e) => {
 $('.close').click(() => {
   $('#mnav, #overlay').removeClass('active');
 });
+
+// Image Slider
+let index = 0;
+
+function nextImg() {
+  if(index !== 3){
+    $(`#p${index}`).attr('class', 'side-img');
+    $(`#p${index+1}`).attr('class', 'main-img');
+    
+    index++;
+  } else {
+    $(`#p${index}`).attr('class', 'side-img');
+    index = 0;
+    $(`#p${index}`).attr('class', 'main-img');
+  }
+}
+
+function prevImg() {
+  if(index !== 0){
+    $(`#p${index}`).attr('class', 'side-img');
+    $(`#p${index-1}`).attr('class', 'main-img');
+  
+    index--;
+  } else {
+    $(`#p${index}`).attr('class', 'side-img');
+    index = 3;
+    $(`#p${index}`).attr('class', 'main-img');
+  }
+}
