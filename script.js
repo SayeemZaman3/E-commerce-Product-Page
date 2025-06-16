122a9450f8ffd4c1120c7bdf8f1f6888fb97546d
SayeemZaman3 <sayeem312@gmail.com>
2025-06-16 17:34:34 +0600

    svg change & bug fixes

--- script.js
+++ script.js
@@ -16,6 +16,17 @@ $('.close').click(() => {
 });
 
 // Image Slider
+
+// Buttons
+$('.prev')
+  .html('<svg width="12" height="18" xmlns="http://www.w3.org/2000/svg"><path d="M11 1 3 9l8 8" stroke="currentColor" stroke-width="3" fill="none" fill-rule="evenodd"/></svg>')
+  .click(prevImg);
+$('.next')
+  .html('<svg width="13" height="18" xmlns="http://www.w3.org/2000/svg"><path d="m2 1 8 8-8 8" stroke="currentColor" stroke-width="3" fill="none" fill-rule="evenodd"/></svg>')
+  .click(nextImg);
+  
+
+// Prev & Next
 let index = 0;
 
 function nextImg() {
@@ -126,6 +137,7 @@ $('#add-to-cart').click(() => {
     createItem("Fall Limited Sneakers Edition", 125, "assets/image-product-1-thumbnail.jpg", parseInt($('#amount').val()));
     $('#cart').addClass('active');
     $('#amount').val(0);
+    amount = 0;
   }
 });
 
@@ -140,7 +152,7 @@ function createItem(name, price, img, amount) {
         <p class="price-tag">$${price} × ${amount}</p>
         <p class="total-price">$${price*amount}</p>
         <button class="del">
-          <img src="assets/icon-delete.svg" alt="del">
+          <svg width="14" height="16" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><path d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4.5 6.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Z" id="a"/></defs><use fill="#C3CAD9" fill-rule="nonzero" xlink:href="#a"/></svg>
         </button>
       </div>
     </div>
@@ -155,11 +167,9 @@ function createItem(name, price, img, amount) {
     $('#totalAmount').text(totalAmount);
     
     $(this).closest('.item').remove();
+    
     cartCheck()
   });
-  
-  totalAmount ? $('#totalAmount').addClass('active') :
-  $('#totalAmount').removeClass('active');
   
   $('#totalAmount').text(totalAmount);
   cartCheck()
@@ -172,6 +182,8 @@ function cartCheck() {
   } else {
     $('#status').removeClass('active');
   }
+  totalAmount ? $('#totalAmount').addClass('active') :
+  $('#totalAmount').removeClass('active');
 }
 cartCheck();
 
